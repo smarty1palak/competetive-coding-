7a179439296093f11cfdf42242c1be4e8b39e159
palak singhal <smarty1palak@gmail.com>
2017-09-29 16:45:30 +0000

    Update XENTASK.cpp

--- codechef-solutions/XENTASK.cpp
+++ codechef-solutions/XENTASK.cpp
@@ -1,1 +1,33 @@
-
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+	int t,n[10],a[10][20000],b[10][20000],s,s1,i,j;
+	scanf("%d",&t);
+	for(i=0;i<t;i++)
+	{
+		scanf("%d",&n[i]);
+		for(j=0;j<n[i];j++)
+		 scanf("%d",&a[i][j]);
+		for(j=0;j<n[i];j++)
+		 scanf("%d",&b[i][j]);
+	}
+	for(i=0;i<t;i++)
+	{
+		for(j=0,s=0;j<n[i];j=j+2)
+			s=s+a[i][j];
+		for(j=1;j<n[i];j=j+2)
+			s=s+b[i][j];
+		for(j=0,s1=0;j<n[i];j=j+2)
+		    s1=s1+b[i][j];
+		for(j=1;j<n[i];j=j+2)
+		    s1=s1+a[i][j];
+		if(s<s1)
+		 printf("%d\n",s);
+		else
+		 printf("%d\n",s1);
+	}
+	return 0;
+}
